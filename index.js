89291e7d317feacfaee9141685fae14fa8b2c195
ilkerortak <ilkerortakofficial@gmail.com>
2026-03-16 03:52:24 +0300

    Initialize Express server with video download feature

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const path = require("path");
+const ytdlp = require("yt-dlp-exec");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static(__dirname));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/download", async (req, res) => {
+  const url = req.query.url;
+  if (!url) return res.send("Video linki gerekli");
+
+  try {
+    const file = "video.mp4";
+    await ytdlp(url, { output: file, format: "mp4" });
+    res.download(file);
+  } catch (err) {
+    res.send("Video indirilemedi");
+  }
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log("Server running on port " + PORT));
